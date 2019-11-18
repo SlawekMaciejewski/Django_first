b92b44961fbfdd2c6a29a24c7c14acde926b3156
SlawekMaciejewski <slawek74.m@interia.pl>
2019-11-18 20:32:44 +0100

    Add new filds to admin site and modify views.py and post_list.html

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'created_date', 'published_date',)
+    list_filter = ('author', 'created_date',)
+    date_hierarchy = 'published_date'
+    raw_id_fields = ('author',)
+
+admin.site.register(Post, PostAdmin)
